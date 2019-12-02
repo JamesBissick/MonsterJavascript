a0e92ce695683e9d8739229b23e2f7209ed8ef20
James Harrys <bissickj@gmail.com>
2019-12-02 13:57:36 +0100

    28 xHR and Fetch

--- 28. xHR and Fetch 3/app.js
+++ 28. xHR and Fetch 3/app.js
@@ -1,1 +1,10 @@
 const button = document.querySelector('button');
+const output = document.querySelector('.output');
+const cost = document.querySelector('input');
+
+button.addEventListener('click', function () {
+    console.log('click');
+    let tip = (cost.value * 0.15).toFixed(2);
+    let temp = `<h1>You should tip $${tip} on $${cost.value}</h1>`;
+    output.innerHTML = temp;
+});
